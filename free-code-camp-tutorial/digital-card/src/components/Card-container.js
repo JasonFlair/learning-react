4fec5fab0b98976db2741a9a763ba7d67ddccbcf
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2023-08-23 14:27:10 +0000

    digital card almost done

--- free-code-camp-tutorial/digital-card/src/components/Card-container.js
+++ free-code-camp-tutorial/digital-card/src/components/Card-container.js
@@ -1,12 +1,16 @@
 import '../App.css';
 import Profile from './Profile';
 import Image from './Image';
+import About from './About';
+import Contact from './Contact';
 
 function CardContainer() {
   return (
     <div className="card-container">
-      <Image />
-      <Profile />
+      < Image />
+      < Profile />
+      <Contact />
+      <About />
     </div>
   );
 }
---
+++ free-code-camp-tutorial/digital-card/src/components/Contact.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Contact() {
+  return (
+    <div className='contact'>
+      <button className='email-button'>Email</button>
+      <button className='linkedin-button'>LinkedIn</button>
+    </div>
+  );
+}
+
+export default Contact;
